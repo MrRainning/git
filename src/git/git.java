f318d08e059b288dc884d35d55deedf5a08fbfe0
MrRainning <1366715002@qq.com>
2018-05-04 20:32:26 +0800

    add a sentence : " i changed this project on 2018-5-4"

--- src/git/git.java
+++ src/git/git.java
@@ -6,5 +6,6 @@ public class git {
 		System.out.println("this is a test of git");
 		System.out.println("file has been changed");
 		System.out.println("what are you going to do when you grow up");
+		System.out.println("i changed this project on 2018-5-4");
 	}
 }
